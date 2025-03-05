bff4f94409751ddf3daaf0f52ae4e4038c53fb36
Robert Russo <rrusso@lsu.edu>
2025-03-05 23:19:19 +0000

    Add a todo note

--- version.php
+++ version.php
@@ -21,6 +21,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+// TODO: Think about my own DB table to store this data.
+
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_wdsprefs';
